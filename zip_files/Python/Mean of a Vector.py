4d9e771bf19cee8b7fdf3b7da3304c10ed0c4ea5
H-J10 <abdihakimibrahim545@gmail.com>
2025-12-04 23:41:31 +0300

    Add extracted contents of R, Python, and Julia ZIPs

---
+++ zip_files/Python/Mean of a Vector.py
@@ -0,0 +1,5 @@
+# Python
+def mean_of_list(lt):
+    return sum(lt)/len(lt)
+
+print(mean_of_list([2, 4, 6, 8]))  # Output: 5.0
---
+++ zip_files/Python/Square Sum Function.py
@@ -0,0 +1,5 @@
+# Python
+def square_sum(x, y):
+    return x**2 + y**2
+
+print(square_sum(3, 4))  # Output: 25
